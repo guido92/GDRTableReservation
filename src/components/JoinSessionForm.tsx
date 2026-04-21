4abf7b09ad054cac450f8ea692c54102e6fee949
Guido92 <michaelguidotti92@gmail.com>
2026-04-21 12:45:27 +0200

    Clean up JoinSessionForm

--- src/components/JoinSessionForm.tsx
+++ src/components/JoinSessionForm.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import { Session } from '@/types';
 import { useRouter } from 'next/navigation';
-import NextLink from 'next/link';
 import Link from 'next/link';
 
 export default function JoinSessionForm({ session }: { session: Session }) {
